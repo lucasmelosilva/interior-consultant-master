642c26f0047026572cd809b9e617d1446d765c9a
Lucas Melo <lucas.melo044@gmail.com>
2022-03-25 09:00:54 -0300

    add animacao do botao do menu no JS

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,25 @@
-const button = document.querySelector('.menu-button');;
+const button = document.querySelector('.menu-button');
+const imageButton = [ './assets/img/svg/menu_white_24dp.svg', './assets/img/svg/close_white_24dp.svg']
 const menu = document.querySelector('.menu');
+const menuItems = document.querySelectorAll('.menu-list_item');
 
 button.addEventListener('click', () => {
   menu.classList.toggle('open');
+  button.attributes.src.value = imageButton[1];
+  imageButton.reverse();
 })
+
+menu.addEventListener('click', (e) => {
+  removeActive()
+  addActive(e.target)
+})
+
+function removeActive() {
+  menuItems.forEach(item => {
+    item.classList.remove('active');
+  })
+}
+
+function addActive(item) {
+  item.classList.add('active');
+}
